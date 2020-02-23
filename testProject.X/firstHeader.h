49fd847ccb6c7f061e7a294d6a95c531a6ca0011
Everton Alves <ealvesgomes89@gmail.com>
2020-02-23 19:09:30 -0300

    Edição do firstHeader.h e inclusão deste arquivo em testProject.c

---
+++ testProject.X/firstHeader.h
@@ -0,0 +1,11 @@
+//Primeiro arquivo header do projeto
+#define GITHUB
+//#define INCLINOMETRO
+
+#ifdef GITHUB
+	char c = 'a';
+	char a = 'c';
+#endif
+#ifdef INCLINOMETRO
+	float angx = 0, angy = 0;
+#endif
--- testProject.X/testProject.c
+++ testProject.X/testProject.c
@@ -1,2 +1,7 @@
 //Projeto Teste - Trabalhando com a plataforma git
 // Quaisquer modificações podem ser realizadas aqui
+#include 'firstHeader.h'
+
+void main(){
+    
+}
